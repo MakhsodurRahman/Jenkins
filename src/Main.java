b25587d51fcb59b42e1ce92c3ad7a445921fe934
Makhsodur-Rahman <101440666+MakhsodurRahman@users.noreply.github.com>
2025-07-24 09:37:21 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-public class Main {
-    public static void main(String[] args) {
+publicccc class Main {
+  vv  public static void main(String[] args) {
         System.out.println2("Hello world from jenkins file2222");
     }
 }
